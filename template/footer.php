46dc55a4e516014508ebde6d5e726afc264d27d3
Rakawee <maulanaraka814@gmail.com>
2025-01-31 05:10:37 +0700

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,26 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>HRIS</span></strong>. All Rights Reserved
+    </div>
+   
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="<?php echo BASE_URL; ?>/assets/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/chart.js/chart.umd.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/echarts/echarts.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/quill/quill.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/tinymce/tinymce.min.js"></script>
+<script src="<?php echo BASE_URL; ?>/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="<?php echo BASE_URL; ?>/assets/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
